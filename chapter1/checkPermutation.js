0cfd1caa6a0f926748824e8bfafab4c39981b9b6
Greg Atria <greg.atria@disney.com>
2019-11-14 21:57:24 -0500

    adding output

--- chapter1/checkPermutation.js
+++ chapter1/checkPermutation.js
@@ -3,6 +3,10 @@ checkPermutation = (str1, str2) => {
   return str1.split("").sort().join("") === str2.split("").sort().join("");
 }
 
+console.log(checkPermutation("fitt", "shitt"))
+console.log(checkPermutation("pantera", "tapnera"))
+console.log(checkPermutation("telly", "ellyt"))
+
 //checkPermutation("fitt", "shitt") => false
 //checkPermutation("pantera", "tapnera") => true
 //checkPermutation("telly", "ellyt") => true
