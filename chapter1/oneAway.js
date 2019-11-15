a7fc0cef043b8fdd8b28c559ba5b5e054c76d23b
Greg Atria <greg.atria@disney.com>
2019-11-15 14:44:25 -0500

    cleanup

--- chapter1/oneAway.js
+++ chapter1/oneAway.js
@@ -4,12 +4,12 @@
 //  Given two strings, write a function to check if they are one edit (or zero
 //  edits) away.
 
-oneAway = (str1, str2) => {
-    var arr = []
+function oneAway(str1, str2){
+    let arr = [];
     function loopStrings(firstStr, compareStr){
-        for (var i = 0; i< firstStr.length; i++){
+        for (let i = 0; i< firstStr.length; i++){
             if (!compareStr.includes(firstStr[i])){
-                arr.push(i)
+                arr.push(i);
             }
         }
         if (arr.length === 1){
@@ -19,17 +19,12 @@ oneAway = (str1, str2) => {
     }
     //replace character or  addedcharacter  firsSte
     if (str1.length === str2.length || str1.length > str2.length){
-       return loopStrings(str1, str2)
+       return loopStrings(str1, str2);
     }
     else if (str2.length > str1.length) {
-       return loopStrings(str2, str1)
+       return loopStrings(str2, str1);
     }
 }
-
-
-// oneAway("pale", "bake") => false
-// oneAway("pale", "ple") => true
-// oneAway("pale", "pales") => true
-// oneAway("pales", "pale") => true
-// oneAway("pale", "bale") => true
-// oneAway("ple", "pale") => true
+console.log(oneAway("pale", "bake")); // false
+console.log(oneAway("pale", "ple")); // true
+console.log(oneAway("pale", "pales")); //true
