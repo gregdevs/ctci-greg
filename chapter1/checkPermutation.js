8b3330bd31fd36a559df46f7a8082ced5ebc9cbb
Greg Atria <greg.atria@disney.com>
2019-11-15 14:39:22 -0500

    changing to function declaration

--- chapter1/checkPermutation.js
+++ chapter1/checkPermutation.js
@@ -1,5 +1,5 @@
 // Given two string, write a method to decide if one is a permutation of the other;
-checkPermutation = (str1, str2) => {
+function checkPermutation (str1, str2) {
   return str1.split("").sort().join("") === str2.split("").sort().join("");
 }
 
