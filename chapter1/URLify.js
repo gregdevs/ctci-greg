c7b83043160694e77512444a2fe00e72d9d3afac
Greg Atria <greg.atria@disney.com>
2019-11-19 21:14:35 -0500

    cleanup

--- chapter1/URLify.js
+++ chapter1/URLify.js
@@ -1,9 +1,7 @@
 // Write a method to replace all spaces in a string with `%20`. You may assume  that the string has sufficient space at the  end to hold the additional characters, and that you are given the "true" length of the string. 
 
 function URLify(str, n){
-    if (str.trim().length === n){
-        return str.trim().replace(/ /g, "%20");
-    }
+    return str.trim().replace(/ /g, "%20");   
 }
 console.log(URLify("Mr John Smith    ", 13))
 console.log(URLify("Hello Everyone  ", 14))
