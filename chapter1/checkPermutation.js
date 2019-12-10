93aea28e2464d63cf43e0187fbae9ae73a9d6072
Greg <gregatria@gmail.com>
2019-12-10 09:41:43 -0500

    Gregdevs/check perm (#1)
    * added first 3 solutions
    
    * adding palindromePerm
    
    * adding initial oneAway, cleaning up palindrome to use a hashTable
    
    * adding better  comments to palindromePerm
    
    * adding stringcompression; cleaning up palindromePern
    
    * added rotateatrix
    
    * cleaning up spacing
    
    * adding output
    
    * removing
    
    * adding checkPerm
    
    * removing
    
    * changing to function declaration

---
+++ chapter1/checkPermutation.js
@@ -0,0 +1,12 @@
+// Given two string, write a method to decide if one is a permutation of the other;
+function checkPermutation (str1, str2) {
+  return str1.split("").sort().join("") === str2.split("").sort().join("");
+}
+
+console.log(checkPermutation("fitt", "shitt"))
+console.log(checkPermutation("pantera", "tapnera"))
+console.log(checkPermutation("telly", "ellyt"))
+
+//checkPermutation("fitt", "shitt") => false
+//checkPermutation("pantera", "tapnera") => true
+//checkPermutation("telly", "ellyt") => true
